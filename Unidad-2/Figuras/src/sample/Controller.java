42c708530df86806e1665257ce8e7c474eaac932
Omar-Amaro <57246187+Omar-Amaro@users.noreply.github.com>
2020-12-10 00:12:15 -0700

    Actividad de evaluación Expresiones regulares
    Se que se ve muy simple pero hay muchas maneras de crear oraciones y lo veo mas para un programa de niños

---
+++ Unidad-2/Figuras/src/sample/Controller.java
@@ -0,0 +1,116 @@
+package sample;
+
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import org.fxmisc.flowless.VirtualizedScrollPane;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
+import org.fxmisc.richtext.model.StyleSpans;
+import org.fxmisc.richtext.model.StyleSpansBuilder;
+import org.reactfx.Subscription;
+
+import java.time.Duration;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static sample.Configs.Configs.*;
+
+public class Controller {
+    @FXML VBox centro;
+    @FXML TextArea consola;
+    private CodeArea codeArea;
+    private ExecutorService executor;
+    @FXML protected void initialize(){
+        executor = Executors.newSingleThreadExecutor();
+        codeArea = new CodeArea();
+        codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
+        Subscription cleanupWhenDone = codeArea.multiPlainChanges()
+                .successionEnds(Duration.ofMillis(500))
+                .supplyTask(this::computeHighlightingAsync)
+                .awaitLatest(codeArea.multiPlainChanges())
+                .filterMap(t -> {
+                    if(t.isSuccess()) {
+                        return Optional.of(t.get());
+                    } else {
+                        t.getFailure().printStackTrace();
+                        return Optional.empty();
+                    }
+                })
+                .subscribe(this::applyHighlighting);
+
+        // call when no longer need it: `cleanupWhenFinished.unsubscribe();`
+
+        codeArea.replaceText(0, 0, sampleCode);
+        StackPane pane=new StackPane(new VirtualizedScrollPane<>(codeArea));
+        centro.getChildren().add(pane);
+        pane.setPrefSize(600,600);
+       //Scene scene = new Scene(new StackPane(new VirtualizedScrollPane<>(codeArea)), 600, 400);
+    }
+    public void compilar(ActionEvent event){
+        String error="";
+        String[] renglones=codeArea.getText().split("\\n");
+        for (int x=0;x<renglones.length;x++){
+            boolean encontro=false;
+            for (int y=0;y<expresiones.length;y++){
+                Pattern p=Pattern.compile(expresiones[y]);
+                Matcher matcher=p.matcher(renglones[x]);
+                if (matcher.matches()){
+                    encontro=true;
+                    y=expresiones.length+1;
+                }
+            }
+            if (!encontro){
+                error+="Error de sintaxis en la linea "+(x+1)+" \n";
+            }
+            consola.setText(error);
+        }
+    }
+
+    private Task<StyleSpans<Collection<String>>> computeHighlightingAsync() {
+        String text = codeArea.getText();
+        Task<StyleSpans<Collection<String>>> task = new Task<StyleSpans<Collection<String>>>() {
+            @Override
+            protected StyleSpans<Collection<String>> call() throws Exception {
+                return computeHighlighting(text);
+            }
+        };
+        executor.execute(task);
+        return task;
+    }
+
+    private void applyHighlighting(StyleSpans<Collection<String>> highlighting) {
+        codeArea.setStyleSpans(0, highlighting);
+    }
+
+    private static StyleSpans<Collection<String>> computeHighlighting(String text) {
+        Matcher matcher = PATTERN.matcher(text);
+        int lastKwEnd = 0;
+        StyleSpansBuilder<Collection<String>> spansBuilder
+                = new StyleSpansBuilder<>();
+        while(matcher.find()) {
+            String styleClass =
+                    matcher.group("KEYWORD") != null ? "keyword" :
+                            matcher.group("PAREN") != null ? "paren" :
+                                    matcher.group("BRACE") != null ? "brace" :
+                                            matcher.group("BRACKET") != null ? "bracket" :
+                                                    matcher.group("SEMICOLON") != null ? "semicolon" :
+                                                            matcher.group("STRING") != null ? "string" :
+                                                                    matcher.group("COMMENT") != null ? "comment" :
+                                                                            null; /* never happens */ assert styleClass != null;
+            spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
+            spansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
+            lastKwEnd = matcher.end();
+        }
+        spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
+        return spansBuilder.create();
+    }
+}
